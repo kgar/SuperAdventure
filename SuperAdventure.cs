9800bb52c3bab58b592729d6fe146f13ecd70983
Joey <joeymonachino@gmail.com>
2021-03-08 17:09:34 -0600

    Add project files.

---
+++ SuperAdventure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+
+            public SuperAdventure()
+        {
+            InitializeComponent();
+
+            _player = new Player();
+
+            //assigning values to the properties in _player object
+
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            //giving values to the form fields: hitpoint, gold, xp, and level. My "invisible" fields start with "lbl"
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+    }
+}
